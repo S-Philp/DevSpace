2bb4259b954760bf39f67015101933f96307258a
S-Philp <shannel.philp@yahoo.com>
2020-12-10 13:48:49 -0600

    First push with CRU functions

---
+++ server/routes/posts.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { getPosts, createPost, updatePost } from '../controllers/posts.js';
+
+
+const router = express.Router();
+
+//http://localhost:5000/posts
+
+router.get('/', getPosts);
+router.post('/', createPost);
+router.patch('/:id', updatePost);
+
+export default router;
