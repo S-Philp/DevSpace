e8f38b36ae262e824a4f12b3405412096eda08d4
S-Philp <shannel.philp@yahoo.com>
2020-12-15 14:27:18 -0600

    deployment prep

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get("/", (req,res) => {
+    res.send('Hello to DevSpace API');
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
